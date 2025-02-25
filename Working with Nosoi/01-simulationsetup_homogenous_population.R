53fbeb395b62a69d02c436848f596053f63dbbce
Sanne-BP <s.b.puister@student.rug.nl>
2025-02-25 19:50:37 +0100

    made a start with simulation set up of homogenous populations

---
+++ Working with Nosoi/01-simulationsetup_homogenous_population.R
@@ -0,0 +1,8 @@
+#######Simulation set up
+rm(list = ls())
+
+#####Spread of a pathogen in a homogeneous population
+#nosoi can accommodate a wide range of epidemiological transmission scenarios. It hence relies on many parameters, that need to be set properly for the right scenario to be simulated. This tutorial aims to illustrate how to set up a nosoi simulation for a “simple” case: a pathogen being transmitted within a population without structure. We will present two cases, first for a single-host, and then a dual-host pathogen.
+
+#Setting up the simulation
+

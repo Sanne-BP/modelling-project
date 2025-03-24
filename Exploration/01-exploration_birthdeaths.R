7fe4e1fe4aa0e0d259c58b04c368aae3db69a391
Sanne-BP <s.b.puister@student.rug.nl>
2025-03-24 21:18:04 +0100

    continued with linking the simulation of popsize with births and deaths to nContact

--- Exploration/01-exploration_birthdeaths.R
+++ Exploration/01-exploration_birthdeaths.R
@@ -104,8 +104,8 @@ plot(1:time_steps, population, type="l", col="blue", lwd=2,
 
 
 
-#can we make like a similar function for this population size? that when you run it each time something different comes out of it?
-#like a function that generates a different population size each time you run it?
+#-------------------------------
+#can we make like a similar function for this population size? that when you run it each time something different comes out of it? like a function that generates a different population size each time you run it?
 simulate_population <- function(time_steps = 100, initial_population = 1000, 
                                 birth_rate = 0.05, death_rate = 0.05) {
   # Storage for population size
@@ -123,16 +123,48 @@ simulate_population <- function(time_steps = 100, initial_population = 1000,
   return(population)
 }
 
-plot(1:length(pop_sim), simulate_population(), type="l", col="blue", lwd=2,
+pop_sim <- simulate_population()
+plot(1:length(pop_sim), pop_sim, type="l", col="blue", lwd=2,
      xlab="Time Steps", ylab="Population Size", main="Population Simulation")
 
 
 
 #-----------------------------link this to nContact
+#Now we have created a fluctuating population size dependent on birth and deaths. So, this influences the number of contacts that can be made.
+#nContact determines how many individuals an infected host interacts with per time step. Works alongside pTrans to determine how many infections actually happen. Can be fixed or dynamic, depending on host behavior, infection stage, environment etc. Higher nContacts = faster potential spread, but only if pTrans is not zero
+
+#So, nContacts now becomes dependent on the population size. So, the bigger the population the more contacts and the smaller the fewer contacts, So, it should be some sort of percentage or something. 
+
+#nContact --> depends on pop_sim --> modify n_contact_fct to depend on current population size
+#but should everything now become dependent on the population size or is only nContact sufficient?
+
+simulate_population <- function(time_steps, initial_population, birth_rate, death_rate) {
+  population <- numeric(time_steps)
+  population[1] <- initial_population
+  
+  for (t in 2:time_steps) {
+    births <- rpois(1, birth_rate * population[t-1])
+    deaths <- rbinom(1, population[t-1], death_rate)
+    population[t] <- population[t-1] + births - deaths
+  }
+  
+  return(population)}
+
+time_steps <- 100
+pop_sim <- simulate_population(time_steps, initial_population = 1000, 
+                               birth_rate = 0.05, death_rate = 0.05)
+
+plot(1:length(pop_sim), pop_sim, type="l", col="blue", lwd=2,
+     xlab="Time Steps", ylab="Population Size", main="Population Simulation")
 
 
-
-
+# Modify n_contact_fct to use population size
+n_contact_fct <- function(t, pop_sim) {
+  current_pop <- pop_sim[min(t, length(pop_sim))]  # Get current population size
+  # Generate a contact number directly proportional to population size
+  n_contacts <- abs(round(rnorm(1, current_pop / 1000, 1), 0))  
+  return(n_contacts)  # No enforced minimum
+  }
 
 
 
@@ -173,4 +205,24 @@ SimulationSingle <- nosoiSim(type="single", popStructure="none",
                              prefix.host="H",
                              print.progress=FALSE)
 #The simulation has run for 40 units of time and a total of 111 hosts have been infected.
+
+library(ggplot2)
+cumulative.table <- getCumulative(SimulationSingle)
+dynamics.table <- getDynamic(SimulationSingle)
+
+p1 <- ggplot(data=cumulative.table, aes(x=t, y=Count)) + 
+  geom_line() + 
+  theme_minimal() + 
+  labs(x="Time (t)",y="Cumulative count of infected hosts")
+
+p2 <- ggplot(data=dynamics.table, aes(x=t, y=Count)) + 
+  geom_line() + 
+  theme_minimal() + 
+  labs(x="Time (t)",y="Number of active infected hosts")
+
+#combine p1, p2 
+install.packages("patchwork")
+library(patchwork)
+p1 + p2 
+
 

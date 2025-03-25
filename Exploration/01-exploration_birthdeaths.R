7213b297361c21774c4ed7cdbf0488b7bf9809f2
Sanne-BP <s.b.puister@student.rug.nl>
2025-03-25 12:49:27 +0100

    linked popsize to nContact and also integrated it into the simulation, which also succesfully runned! (hopefully its correct)

--- Exploration/01-exploration_birthdeaths.R
+++ Exploration/01-exploration_birthdeaths.R
@@ -1,4 +1,5 @@
 ####################Trying out some approaches to integrate population dynamics###################
+rm(list = ls())
 
 #----------simplified approach to implement basic birth-death dynamics:
 host_availability <- function(t, current_ids, total_pop_size, birth_rate, death_rate) {
@@ -62,14 +63,7 @@ SimulationSingle <- nosoiSim(type="single", popStructure="none",
                              param.hostAvailability = list(
                                birth_rate=0.05, 
                                death_rate=0.03))
-
-
-###---------------------DOES NOT WORK,FIX THIS!!!!
-
-
-
-
-
+###---------------------DOES NOT WORK
 
 
 
@@ -151,22 +145,99 @@ simulate_population <- function(time_steps, initial_population, birth_rate, deat
   return(population)}
 
 time_steps <- 100
-pop_sim <- simulate_population(time_steps, initial_population = 1000, 
+pop_size <- simulate_population(time_steps, initial_population = 1000, 
                                birth_rate = 0.05, death_rate = 0.05)
 
-plot(1:length(pop_sim), pop_sim, type="l", col="blue", lwd=2,
+plot(1:length(pop_size), pop_size, type="l", col="blue", lwd=2,
      xlab="Time Steps", ylab="Population Size", main="Population Simulation")
 
 
 # Modify n_contact_fct to use population size
-n_contact_fct <- function(t, pop_sim) {
-  current_pop <- pop_sim[min(t, length(pop_sim))]  # Get current population size
+n_contact_fct <- function(t, pop_size) {
+  current_pop <- pop_size[min(t, length(pop_size))]  # Get current population size
   # Generate a contact number directly proportional to population size
   n_contacts <- abs(round(rnorm(1, current_pop / 1000, 1), 0))  
-  return(n_contacts)  # No enforced minimum
+  return(n_contacts)  
   }
 
+#but now it calculates the number of contacts for a population as a whole!! That's not what we want. In nosoi it also calculates it for each individually infected host. So, how many contacts does each individually infected host within the population make? 
+n_contact_fct = function(t){abs(round(rnorm(1, 0.5, 1), 0))} #this is the original function, so this one needs to be adjusted 
 
+#next try:
+n_contact_fct <- function(t, pop_size) {
+  current_pop <- pop_size[min(t, length(pop_size))]  # Get current population size
+  # Generate a contact number directly proportional to population size
+  n_contacts <- abs(round(rnorm(1, current_pop / 1000, 1), 0))  
+  return(rep(n_contacts, current_pop))  
+  }
+
+#This function is not correct because it returns a vector of the same value repeated current_pop times. return(rep(n_contacts, current_pop)) → This creates a vector with the same contact number repeated for every individual in the population, rather than per infected individual.
+
+n_contact_fct <- function(t, pop_sim) {
+  current_pop <- pop_sim[min(t, length(pop_sim))]  # Get current population size
+  base_rate <- 0.5  # Base mean contacts per individual (from original function)
+  # Scale mean contacts per individual based on population size
+  scaled_mean <- base_rate * (current_pop / 1000)  
+  # Each infected individual gets their own contact number (stochastic)
+  n_contacts_i <- abs(round(rnorm(1, scaled_mean, 1), 0))  
+  return(n_contacts_i)  
+}
+
+#this should be the right fix, as each individual gets their own contact number. also maintains stochasticity, contacts fluctuate at each time step + it scales with population size. This way, Nosoi still correctly assigns individual contacts per infected host, while ensuring that contact rates scale properly with population size.
+
+#----------------------------now trying to integrate this part into the nosoi model (example model below in this script)
+n_contact_fct <- function(t) {
+  current_pop <- pop_sim[min(t, length(pop_sim))]  # Get current population size
+  base_rate <- 0.5  
+  scaled_mean <- base_rate * (current_pop / 1000)  
+  n_contacts_i <- abs(round(rnorm(1, scaled_mean, 1), 0))  
+  return(n_contacts_i)  
+}
+
+# Other functions remain the same
+p_Exit_fct  <- function(t) { return(0.08) }
+
+p_Trans_fct <- function(t, p_max, t_incub) {
+  if (t < t_incub) { p <- 0 }
+  if (t >= t_incub) { p <- p_max }
+  return(p)
+}
+
+t_incub_fct <- function(x) { rnorm(x, mean = 7, sd = 1) }
+p_max_fct <- function(x) { rbeta(x, shape1 = 5, shape2 = 2) }
+
+param_pTrans = list(p_max = p_max_fct, t_incub = t_incub_fct)
+
+# Run the Nosoi simulation
+set.seed(805)
+SimulationSingle <- nosoiSim(type = "single", popStructure = "none",
+                             length.sim = time_steps, max.infected = 100, init.individuals = 1,
+                             nContact = n_contact_fct,
+                             param.nContact = NA,
+                             timeDep.nContact = FALSE,
+                             pExit = p_Exit_fct,
+                             param.pExit = NA,
+                             timeDep.pExit = FALSE,
+                             pTrans = p_Trans_fct,
+                             param.pTrans = param_pTrans,
+                             timeDep.pTrans = FALSE,
+                             prefix.host = "H",
+                             print.progress = FALSE)
+
+#well it does run! Now gives the same result as before, because of the set.seed
+SimulationSingle <- nosoiSim(type = "single", popStructure = "none",
+                             length.sim = time_steps, max.infected = 100, init.individuals = 1,
+                             nContact = n_contact_fct,
+                             param.nContact = NA,
+                             timeDep.nContact = FALSE,
+                             pExit = p_Exit_fct,
+                             param.pExit = NA,
+                             timeDep.pExit = FALSE,
+                             pTrans = p_Trans_fct,
+                             param.pTrans = param_pTrans,
+                             timeDep.pTrans = FALSE,
+                             prefix.host = "H",
+                             print.progress = FALSE)
 
 
 
@@ -221,7 +292,6 @@ p2 <- ggplot(data=dynamics.table, aes(x=t, y=Count)) +
   labs(x="Time (t)",y="Number of active infected hosts")
 
 #combine p1, p2 
-install.packages("patchwork")
 library(patchwork)
 p1 + p2 
 

226994bba725666bf38e6319c4c3e2098e5a11da
Sanne-BP <s.b.puister@student.rug.nl>
2025-03-28 15:43:48 +0100

    added logistic growth equation pop dynamics

--- Exploration/00-exploration.R
+++ Exploration/00-exploration.R
@@ -2,7 +2,7 @@
 #clear environment
 rm(list=ls())
 
-#---------set up population dynamics
+#---------set up population dynamics (not using this)
 #Parameters
 population_size <- 1000  # Initial population size
 birth_rate <- 0.02  # Birth rate (constant)
@@ -63,3 +63,54 @@ for (t in 1:time_steps) {
 plot(1:time_steps, infected_data, type = "l", col = "red", 
      xlab = "Time Step", ylab = "Number of Infected Individuals", main = "Epidemic Spread")
 
+
+
+
+
+
+
+#-----------------------trying out another population dynamics script
+#Discrete-time Logistic Population Growth Simulations
+#August 15, 2014
+#By Stephen B. Heard
+#Non-commercial use permitted.
+
+#Set number of generations to plot
+PlotGen <- 200
+
+###Set parameters for simulation 1###
+
+# set initial population size
+N0 <- 10
+
+#Set R
+R <- 3
+
+#Set K
+K <- 500
+
+###Run simulation###
+
+# initialize vector to hold results 
+PopSize <- N0 
+
+# create variable to hold the current population size
+PopNow <- N0 
+
+# calculate population sizes and append to popsize
+for(i in 1:PlotGen) { 
+  PopNow <- PopNow + PopNow*R*(1-PopNow/K)    #discrete logistic
+  if (PopNow < 0) {PopNow <- 0}
+  PopSize <- c(PopSize,PopNow)                #add result to vector
+}
+
+
+###Plot results###
+
+tvals <- 1:length(PopSize) #create a vector of time values equal to length of results vector
+
+plot(tvals[1:PlotGen], PopSize[1:PlotGen],type="o",col="red", 
+     xlab="Generation",ylab="Population size",pch=16,cex=.75)
+
+
+#--> logistic growth equation dynamics, good to think about later!!

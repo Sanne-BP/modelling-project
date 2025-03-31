23d6de2fc756fad3cbf1dc95992c8e49dcd2a627
Sanne-BP <s.b.puister@student.rug.nl>
2025-03-31 12:48:27 +0200

    new script

---
+++ Exploration/03-dynamics_birthsdeaths.R
@@ -0,0 +1,5 @@
+#Right now we have: a working simulation in which nContact is determined based on population size dynamics. But population size dynamics are not being influenced by the simulation itself, they are not dependent on each other yet. 
+#     •	So we have a working population dynamics system, in which there is a given population size          which is influenced over time by birth and death rates
+#     •	nContact is based on this population size, each individual in the simulation gets their own         contact number 
+
+

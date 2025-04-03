c7f28b7451060007c17f08bf0cdf90a089a662cc
Sanne-BP <s.b.puister@student.rug.nl>
2025-04-03 16:16:33 +0200

    update + new script for altering/updating and exploring pop dynamics

---
+++ Exploration/04-updating_birthsdeaths.R
@@ -0,0 +1,3 @@
+#Next step is to let the population dynamics start at t = 0, as the nosoi simulation also starts from t = 0, right now my population dynamics start at t = 1!
+#After that, try to model it with more extreme parameters to actually see if it works. For now I have been keeping everything very small and realistic, this way changes cannot even be seen.
+
